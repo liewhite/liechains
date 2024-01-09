3ceeb61f04a998dfb40ec0d5847e626a774982bb
李霖 <leeliewhite@gmail.com>
2024-01-09 16:52:54 +0800

    wallet from keystore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="liechains",
-    version="0.0.8",
+    version="0.0.9",
     author="leeliewhite",
     author_email="leeliewhite@gmail.com",
     description="toolkit for blockchains",
