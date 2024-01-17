108e61467c5756a5723e86c4c14a68f78bbf1389
李霖 <leeliewhite@gmail.com>
2024-01-17 14:44:40 +0800

    0.2.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="liechains",
-    version="0.2.5",
+    version="0.2.6",
     author="leeliewhite",
     author_email="leeliewhite@gmail.com",
     description="toolkit for blockchains",
