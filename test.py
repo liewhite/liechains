c5fa4032588f4513f8fb847de526f38df9e11b8e
李霖 <leeliewhite@gmail.com>
2024-05-16 10:51:31 +0800

    limit order

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from web3 import Web3
 from ether.client import Web3Client
+from ether.dex.limit_order import get_quotes
 from ether.dex.univ3 import Univ3Swap
 from ether.middlewares.remote_signer import RemoteSigner
 from rlp import encode
@@ -8,7 +9,6 @@ from ether.dex.univ2 import *
 
 
 if __name__ == "__main__":
-
     # cli = Web3Client["local"].with_account('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80')
     # v2 = Univ2Swap(
     #     cli,
@@ -19,17 +19,18 @@ if __name__ == "__main__":
     # )
     # print(v2.quote(10 ** 18))
     # print(v2.swap(10 ** 18, 0).hex())
-
     cli = Web3Client["local"].with_account('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80')
     v3 = Univ3Swap(
         cli,
         "0x1F98431c8aD98523631AE4a59f267346ea31F984",
         "0xE592427A0AEce92De3Edee1F18E0157C05861564",
         "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6",
-        "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-        "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+        "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+        "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
         3000
     )
+    dex, amount_price = get_quotes([v3], 250000000000000000000, 0, 100 * 10 ** 18)[0]
+    print(dex.swap(amount_price[0],0).hex())
 
-    print(v3.quote(10 ** 18))
-    print(v3.swap(10 ** 18, 0).hex())
+    # print(v3.quote(10 ** 18))
+    # print(v3.swap(10 ** 18, 0).hex())
