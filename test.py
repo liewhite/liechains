021e870367b0401c0f67c7975e0bf16c0c13eec9
李霖 <leeliewhite@gmail.com>
2024-05-20 14:45:23 +0800

    balancer

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from web3 import Web3
 from ether.client import Web3Client
+from ether.dex.balancer import Balancer
 from ether.dex.limit_order import get_quotes
 from ether.dex.univ3 import Univ3Swap
 from ether.middlewares.remote_signer import RemoteSigner
@@ -19,18 +20,27 @@ if __name__ == "__main__":
     # )
     # print(v2.quote(10 ** 18))
     # print(v2.swap(10 ** 18, 0).hex())
-    cli = Web3Client["local"].with_account('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80')
-    v3 = Univ3Swap(
-        cli,
-        "0x1F98431c8aD98523631AE4a59f267346ea31F984",
-        "0xE592427A0AEce92De3Edee1F18E0157C05861564",
-        "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6",
-        "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
-        "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
-        3000
+
+    cli = Web3Client["local"].with_account(
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
     )
-    dex, amount_price = get_quotes([v3], 250000000000000000000, 0, 100 * 10 ** 18)[0]
-    print(dex.swap(amount_price[0],0).hex())
+    dex = Balancer(cli, cli.to_checksum_address('0xba12222222228d8ba445958a75a0704d566bf2c8'),'0x596192bb6e41802428ac943d2f1476c1af25cc0e000000000000000000000659', '0xbf5495Efe5DB9ce00f80364C8B423567e58d2110')
+    amount_out = dex.quote(10 ** 18)
+    dex.swap(10 ** 18, amount_out-1)
+
+
+    # cli = Web3Client["local"].with_account('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80')
+    # v3 = Univ3Swap(
+    #     cli,
+    #     "0x1F98431c8aD98523631AE4a59f267346ea31F984",
+    #     "0xE592427A0AEce92De3Edee1F18E0157C05861564",
+    #     "0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6",
+    #     "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1",
+    #     "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
+    #     3000
+    # )
+    # dex, amount_price = get_quotes([v3], 250000000000000000000, 0, 100 * 10 ** 18)[0]
+    # print(dex.swap(amount_price[0],0).hex())
 
     # print(v3.quote(10 ** 18))
     # print(v3.swap(10 ** 18, 0).hex())
